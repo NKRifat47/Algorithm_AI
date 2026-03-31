17466770e0595d547a0a34b666fdabc73706095f
NKRifat47 <nk.rifat47@gmail.com>
2026-04-01 03:39:41 +0600

    Initial setup 2

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,18 +1,8 @@
 import { Router } from "express";
-import { AuthRouter } from "../modules/auth/auth.route.js";
 import { OtpRouter } from "../modules/otp/otp.route.js";
-import { UserRoutes } from "../modules/user/user.route.js";
 
 export const router = Router();
 const moduleRoutes = [
-  {
-    path: "/user",
-    route: UserRoutes,
-  },
-  {
-    path: "/auth",
-    route: AuthRouter,
-  },
   {
     path: "/otp",
     route: OtpRouter,
